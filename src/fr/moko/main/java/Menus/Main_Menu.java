d7f0290fcee19b176ffcc120ee043802111da593
MokoMaracucho <desmoulin.federico@gmail.com>
2019-02-28 10:59:06 -0500

    Main_Menu

---
+++ src/fr/moko/main/java/Menus/Main_Menu.java
@@ -0,0 +1,18 @@
+package fr.moko.main.java.Menus;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
+/**
+ * Menu principal
+ *
+ * @author : Moko
+ * @version : 1.0
+ */
+public class Main_Menu {
+
+    // Instance : "Logger"
+    static Logger logger = LogManager.getRootLogger();
+}
