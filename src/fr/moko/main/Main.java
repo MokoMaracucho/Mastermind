b3349ae929b303915305a140227032f217698ef5
MokoMaracucho <desmoulin.federico@gmail.com>
2019-02-28 10:52:19 -0500

    Main - Log4j2

---
+++ src/fr/moko/main/Main.java
@@ -0,0 +1,17 @@
+package fr.moko.main;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Class "Main {}"
+ *
+ * @author : Moko
+ * @version : 1.0
+ */
+public class Main {
+
+    // Instance : "Logger"
+    static Logger logger = LogManager.getRootLogger();
+}
