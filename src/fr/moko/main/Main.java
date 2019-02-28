05e1d47a75edb04ff3a8c7ae3c8a1c234ac59bb0
MokoMaracucho <desmoulin.federico@gmail.com>
2019-02-28 10:55:00 -0500

    main - Niveaux log4j2

--- src/fr/moko/main/Main.java
+++ src/fr/moko/main/Main.java
@@ -1,6 +1,5 @@
 package fr.moko.main;
 
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,4 +13,21 @@ public class Main {
 
     // Instance : "Logger"
     static Logger logger = LogManager.getRootLogger();
+
+    /**
+     * Méthode "main(String[] args)"
+     *
+     * @param args : ...
+     */
+    public static void main(String[] args) {
+
+        // Loggers
+        logger.trace("msg de trace"); // E/S methods
+        logger.debug("msg de debogage"); // Display values of datas
+        logger.info("msg d'information"); //loading of conf file, beg/end long process
+        logger.warn("msg d'avertissement"); // error login, invalid data
+        logger.error("msg d'erreur"); // all throwned exceptions
+        logger.fatal("msg d'erreur fatale");  // error DB, exceptions which stop the program
+        System.out.print("\n");
+    }
 }
